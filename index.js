cc2b27f46be795149fc286185886d054164a0ca3
joel <joel@localhost.localdomain>
2019-05-10 12:40:39 -0500

    Se realizo la configuracion del puerto wq

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 
 // imporar los queries 
-const express = require('express')
-const bodyParser = require('body-parser')
-const app = express()
-const db = require('./queries')
-const port = 3000
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const db = require('./queries');
+//const port = 3000;
+const port = process.env.PORT || 5000
+
+
 
 //es un middleware que serializa los cuerpos de las respuestas 
 //   para poder invocar response.param
@@ -17,18 +20,18 @@ app.use(
 
 
 //usar los queries importados 
-app.post('/login', db.login)
-app.get('/users', db.getUsers)
-app.get('/users/:id', db.getUserById)
-app.post('/users', db.createUser)
-app.put('/users/:id', db.updateUser)
-app.delete('/users/:id', db.deleteUser)
+app.post('/login', db.login);
+app.get('/users', db.getUsers);
+app.get('/users/:id', db.getUserById);
+app.post('/users', db.createUser);
+app.put('/users/:id', db.updateUser);
+app.delete('/users/:id', db.deleteUser);
 
 app.get('/', (request, response) => {
 	response.json({ info: 'Node.js, Express, and Postgres API' })
 });
 
 app.listen(port, () => {
-	console.log(`App running on port ${port}.`)
+	console.log(`App corriendo en el puerto ${port}.`)
 });
 
