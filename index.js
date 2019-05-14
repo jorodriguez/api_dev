ab468750eafe9cbee88c5171fb303684701b61da
joel rodriguez <joel.rod.roj@hotmail.com>
2019-05-14 11:57:29 -0500

    Cambios por el COORD

--- index.js
+++ index.js
@@ -8,18 +8,20 @@ const port = process.env.PORT || 5000;
 
 //es un middleware que serializa los cuerpos de las respuestas 
 //   para poder invocar response.param
+app.use((req, res, next) => {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+    res.append('Access-Control-Allow-Origin', '*');
+    res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.append('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
 app.use(bodyParser.json())
 app.use(
 	bodyParser.urlencoded({
 		extended: true,
 	})
 );
-app.use((req, res, next) => {
-    res.append('Access-Control-Allow-Origin', ['*']);
-    res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.append('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-})
 
 //usar los queries importados 
 app.post('/login', db.login);
