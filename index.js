389d9a542742d2111946866295ad26074fec4911
joel <joel@localhost.localdomain>
2019-05-14 19:43:43 -0500

    cambios en index

--- index.js
+++ index.js
@@ -10,11 +10,11 @@ const port = process.env.PORT || 5000;
 
 app.use(function(req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 	next();
-});
+  });
   
-
 
 //es un middleware que serializa los cuerpos de las respuestas 
 //   para poder invocar response.param
