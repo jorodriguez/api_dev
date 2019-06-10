a93b2ccfa6c5c42646d906925b7a5d21a1723ec8
joel <joel@localhost.localdomain>
2019-06-10 16:05:46 -0500

    Cambios

--- services/balance_alumno.js
+++ services/balance_alumno.js
@@ -15,111 +15,28 @@ const pool = new Pool({
     ssl: { rejectUnauthorized: false }
 });
 
-//obtener actividades
-const getCatalogoActividades = (request, response) => {
-    console.log("@getCatalogoActividades");
+
+const registrarBalanceAlumno = (id_alumno,genero) => {
+    console.log("@ragistrarBalanceAlumno");
     try {
-        var validacion = helperToken.validarToken(request);
-
-        if (!validacion.tokenValido) {
-            return response.status(validacion.status).send(validacion.mensajeRetorno);;
-        }
-
-        pool.query(
-            "WITH actividades AS( "+
-            "   SELECT a.id,"+
-            "       a.nombre,"+
-            "       a.posicion,"+
-            "       a.icono,"+
-            "    ("+
-            "        select array_to_json("+
-            "            (select array_agg(ta.*) from cat_tipo_actividad ta where ta.cat_actividad =  a.id )"+
-            "         )"+
-            "    ) as tipo_actividad,"+
-            "    ("+
-            "        select array_to_json("+
-            "            (select array_agg(ta.*) from cat_sub_actividad ta where ta.cat_actividad =  a.id )"+
-            "         )"+
-            "    ) as sub_actividad		"+
-            " FROM cat_actividad a"+
-            " WHERE a.eliminado = false "+
-            " ORDER BY a.posicion ASC"+
-            " ) select array_to_json(array_agg(a.*))  as catalogo_actividades "+
-            "   FROM actividades a",            
+                     
+        pool.query("select insertar_balance_alumno($1,$2)",                               
+            [id_alumno,genero],
             (error, results) => {
                 if (error) {
-                    handle.callbackError(error, response);
-                    return;
-                }
-                response.status(200).json(results.rows);
-            });
-    } catch (e) {
-        handle.callbackErrorNoControlado(e, response);
-    }
-};
-
-
-const registrarActividad = (request, response) => {
-    console.log("@registrarActividad");
-    try {
-        var validacion = helperToken.validarToken(request);
-
-        if (!validacion.tokenValido) {
-            return response.status(validacion.status).send(validacion.mensajeRetorno);;
-        }
-
-        const { alumnosIds,cat_actividad,tipo_actividad,sub_actividad,nota,genero } = request.body;
-
-        console.log("=====>> "+JSON.stringify(request.body));
-             
-        var nullOrEmpty = (val)=>{ 
-            if(val === null || val === -1 || val==='' || val===undefined) 
-            return null
-            else return val;
-        };
-        var nullOrEmptyStr = (val)=>{ 
-            if(val === null || val === -1 || val==='' || val===undefined) 
-            return ''
-            else return val;
-        };
-
-        var sqlComplete = " VALUES ";
-        for (var i = 0; i < alumnosIds.length; i++) {
-            if (i > 0) {
-                sqlComplete += ",";
-            }                      
-            sqlComplete += "(" + alumnosIds[i] + ","+
-                            cat_actividad+","+
-                            nullOrEmpty(tipo_actividad)+","+
-                            nullOrEmpty(sub_actividad)+","+
-                            "current_date,"+
-                            "getHora(''),"+
-                            "'"+nullOrEmptyStr(nota)+"',"+
-                            "'',"+
-                            genero
-                            +")";
-        };
-
-        console.log(" SQL "+sqlComplete);
-
-        pool.query("INSERT INTO co_registro_actividad(co_alumno,cat_actividad,cat_tipo_actividad,cat_sub_actividad,fecha,hora,nota,url_foto,genero) " +            
-            sqlComplete,
-            (error, results) => {
-                if (error) {
-                    handle.callbackError(error, response);
-                    return;
+                    console.log("error al invocar el procedimeinto de alta de balance "+error);
+                    return false;
                 }                
-                mensajeria.enviarMensaje("Actividad ",(nota==null || nota=='' ? 'sin nota':nota));
-                response.status(200).json(results.rowCount)
+                console.log("Se creo el balance y se relaciono ");                
+                return results.rowCount >0;                
             });
     } catch (e) {
-        handle.callbackErrorNoControlado(e, response);
-        
+        console.log("error no controlado  "+error);     
+        return false;        
     }
 };
 
 
 module.exports = {
-    getCatalogoActividades,
-    registrarActividad  
+    registrarBalanceAlumno
 }
