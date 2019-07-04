cdfeee4073faedef5fb9ce0631fc86c855bc4ff3
joel rodriguez <joel.rod.roj@hotmail.com>
2019-07-04 17:53:08 -0500

    Cambios listos para prod

--- services/asistencia.js
+++ services/asistencia.js
@@ -68,7 +68,33 @@ const getAlumnosPorRecibir = (request, response) => {
 
         const id_sucursal = parseInt(request.params.id_sucursal);
 
-        pool.query("SELECT a.*" +
+        pool.query(
+            `SELECT 
+                    grupo.nombre as nombre_grupo,
+                    false as visible,
+					a.*
+             FROM co_alumno a INNER JOIN co_grupo grupo ON a.co_grupo = grupo.id		
+              WHERE a.id not in (
+                           SELECT asistencia.co_alumno
+                               FROM co_asistencia asistencia inner join co_alumno alumno on asistencia.co_alumno=alumno.id            
+                               WHERE asistencia.hora_salida is null and  asistencia.eliminado = false 
+              AND alumno.co_sucursal = $1
+              AND asistencia.eliminado=false
+            ) 
+             AND a.co_sucursal = $2
+             AND a.eliminado = false 
+             ORDER BY a.nombre ASC
+            `,
+        [id_sucursal, id_sucursal],
+        (error, results) => {
+            if (error) {
+                handle.callbackError(error, response);
+                return;
+            }
+            response.status(200).json(results.rows);
+        });
+
+       /* pool.query("SELECT a.*" +
             " FROM co_alumno a " +
             "  WHERE id not in (" +
             "               SELECT asistencia.co_alumno" +
@@ -88,13 +114,55 @@ const getAlumnosPorRecibir = (request, response) => {
                     return;
                 }
                 response.status(200).json(results.rows);
-            });
+            });*/
     } catch (e) {
         handle.callbackErrorNoControlado(e, response);
     }
 };
 
 
+const registrarEntradaAlumnos = (request, response) => {
+    console.log("@registrarEntrada");
+    try {
+        var validacion = helperToken.validarToken(request);
+
+        if (!validacion.tokenValido) {
+            return response.status(validacion.status).send(validacion.mensajeRetorno);;
+        }
+
+        const { ids, genero } = request.body;
+
+        var idsAlumnos = '';
+        var first = true;
+
+        ids.forEach(element => {
+            if (first) {
+                idsAlumnos += (element + "");
+                first = false;
+            } else {
+                idsAlumnos += (',' + element);
+            }
+        });
+
+        console.log("Ids registrar entrada  "+idsAlumnos);
+
+        pool.query("select registrar_entrada_alumno('"+idsAlumnos+"',"+genero+");",
+            (error, results) => {
+                if (error) {
+                    handle.callbackError(error, response);
+                    return;
+                }
+
+                response.status(200).json(results.rowCount)
+            });
+            
+    } catch (e) {
+        handle.callbackErrorNoControlado(e, response);
+
+    }
+};
+
+/*
 const registrarEntradaAlumnos = (request, response) => {
     console.log("@registrarEntrada");
     try {
@@ -129,11 +197,12 @@ const registrarEntradaAlumnos = (request, response) => {
 
                 response.status(200).json(results.rowCount)
             });
+            
     } catch (e) {
         handle.callbackErrorNoControlado(e, response);
 
     }
-};
+};*/
 
 const registrarSalidaAlumnos = (request, response) => {
     console.log("@registrarSalidaAlumnos");
@@ -149,18 +218,6 @@ const registrarSalidaAlumnos = (request, response) => {
         const { ids, genero } = request.body;
 
         console.log("IDS recibidos " + ids);
-
-
-        var sqlComplete = " ( ";
-        for (var i = 0; i < ids.length; i++) {
-
-            if (i > 0) {
-                sqlComplete += ",";
-            }
-            sqlComplete += "" + ids[i];
-        };
-        sqlComplete += ")";
-
         // obtener para el proceso de horas extras
         var idsForHorasExtras = '';
         var first = true;
