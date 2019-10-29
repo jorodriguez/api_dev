bc7383d981cd797afa05d49c2e0add04e6cef6d3
joel.rod.roj@hotmail.com <joel.rod.roj@hotmail.com>
2019-10-28 22:26:34 -0600

    Cambios para actualizar

--- config/ambiente.js
+++ config/ambiente.js
@@ -2,7 +2,7 @@
 
 //Cambiar atributo enviar_mensajes : true, si se va a produccion
 //env : 'PRODUCTION', si se va a produccion
-
+/*
 module.exports = {
     configuracion: {
         'secret': 'supersecret',
@@ -10,16 +10,15 @@ module.exports = {
         enviar_mensajes: false
     }
 };
+*/
 
 
-/*
 module.exports = {
     configuracion: {
         'secret': 'supersecret',
         env: '"PRODUCTION"',
         enviar_mensajes: true
     }
-};*/
-
+};
 
 //psql --set=sslmode=require -h ec2-54-243-197-120.compute-1.amazonaws.com -p 5432 -U vbkxmhcwhsnoxe -d d3cledlk3uuch6
