f2f6d39fba546f63799ea7a42f2998b8367cf8e0
joel <joel@localhost.localdomain>
2019-11-18 21:41:06 -0600

    Cambios en la conexion

--- config/ambiente.js
+++ config/ambiente.js
@@ -2,7 +2,7 @@
 
 //Cambiar atributo enviar_mensajes : true, si se va a produccion
 //env : 'PRODUCTION', si se va a produccion
-
+/*
 module.exports = {
     configuracion: {
         'secret': 'supersecret',
@@ -10,9 +10,9 @@ module.exports = {
         enviar_mensajes: false
     }
 };
+*/
 
 
-/*
 module.exports = {
     configuracion: {
         'secret': 'supersecret',
@@ -20,6 +20,6 @@ module.exports = {
         enviar_mensajes: true
     }
 };
-*/
+
 
 //psql --set=sslmode=require -h ec2-54-243-197-120.compute-1.amazonaws.com -p 5432 -U vbkxmhcwhsnoxe -d d3cledlk3uuch6
