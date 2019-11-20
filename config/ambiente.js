1311514cb8c66f2d4bbc09840e6b8b309bb7c2df
joel rodriguez <joel.rod.roj@hotmail.com>
2019-11-20 16:33:23 -0600

    Subiendo al ambiente de pruebas

--- config/ambiente.js
+++ config/ambiente.js
@@ -2,17 +2,17 @@
 
 //Cambiar atributo enviar_mensajes : true, si se va a produccion
 //env : 'PRODUCTION', si se va a produccion
-/*
+
 module.exports = {
     configuracion: {
         'secret': 'supersecret',
         env: '"DEV"',
         enviar_mensajes: false
     }
-};*/
+};
 
 
-
+/*
 module.exports = {
     configuracion: {
         'secret': 'supersecret',
@@ -20,5 +20,5 @@ module.exports = {
         enviar_mensajes: true
     }
 };
-
+*/
 //psql --set=sslmode=require -h ec2-54-243-197-120.compute-1.amazonaws.com -p 5432 -U vbkxmhcwhsnoxe -d d3cledlk3uuch6
