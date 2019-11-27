8347a6e0cfc0e22f10b66def43c781e307153817
joel.rod.roj@hotmail.com <joel.rod.roj@hotmail.com>
2019-11-26 21:36:38 -0600

    Cambios en la conexion

--- db/conexion.js
+++ db/conexion.js
@@ -2,7 +2,7 @@
 const Pool = require('pg').Pool
 
 //const { dbParams } = require('../config/config');
-
+/*
 const dbParams = {
     user: 'vbkxmhcwhsnoxe',
     host: 'ec2-54-243-197-120.compute-1.amazonaws.com',
@@ -11,10 +11,10 @@ const dbParams = {
     port: 5432,
     ssl: { rejectUnauthorized: false }
 }
-
+*/
 
 //db desarrollo
-/*
+
 const dbParams = {
     user: 'pffyesodvpvsrp',
     host: 'ec2-174-129-242-183.compute-1.amazonaws.com',
@@ -22,7 +22,7 @@ const dbParams = {
     password: 'f4de35950e23261169a79f8ac3007630aaefc8ff887c147b9283a8f68b165019',
     port: 5432,
     ssl: { rejectUnauthorized: false }
-}*/
+}
 
 //const dbParams = dbParamsDev;
 
