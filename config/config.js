46431349f0fb9fceba3644ca84628a1fa7503292
joel.rod.roj@hotmail.com <joel.rod.roj@hotmail.com>
2019-11-26 21:28:16 -0600

    Cambios en la conexion

--- config/config.js
+++ config/config.js
@@ -9,7 +9,7 @@ dotenv.config();
 //heroku pg:psql postgresql-sinuous-19615 --app develop1
 
 //BASE DE DATOS DE DESARROLLO
-
+/*
 module.exports = {    
     dbParams:{
         user: 'pffyesodvpvsrp',
@@ -20,10 +20,10 @@ module.exports = {
         ssl: { rejectUnauthorized: false }
     },
     'secret': 'supersecret'   
-};
+};*/
 
 //BASE DE DATOS DE PRODUCCION
-/*
+
 module.exports = {
     dbParams:{
         user: 'vbkxmhcwhsnoxe',
@@ -34,7 +34,7 @@ module.exports = {
         ssl: { rejectUnauthorized: false }
     },
     'secret': 'supersecret'
-};*/
+};
 
 //conect con psql
 
