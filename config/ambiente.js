da2f03db282a17b7058842586607d55dc25da440
jorodriguez <joel.rod.roj@hotmail.com>
2019-12-19 09:16:51 -0600

    Cambios despues de merge con master

--- config/ambiente.js
+++ config/ambiente.js
@@ -2,16 +2,16 @@
 
 //Cambiar atributo enviar_mensajes : true, si se va a produccion
 //env : 'PRODUCTION', si se va a produccion
-/*
+
 module.exports = {
     configuracion: {
         'secret': 'supersecretDevelop',
         env: '"DEV"',
         enviar_mensajes: false
     }
-};*/
+};
 
-
+/*
 module.exports = {
     configuracion: {
         'secret': 'supersecret',
@@ -19,5 +19,5 @@ module.exports = {
         enviar_mensajes: true
     }
 };
-
+*/
 //psql --set=sslmode=require -h ec2-54-243-197-120.compute-1.amazonaws.com -p 5432 -U vbkxmhcwhsnoxe -d d3cledlk3uuch6
