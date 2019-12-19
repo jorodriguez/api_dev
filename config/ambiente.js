49afdf2ccf7b4aa12eb61b9b33c6bd9ba40e400f
jorodriguez <joel.rod.roj@hotmail.com>
2019-12-19 11:59:23 -0600

    Cambios

--- config/ambiente.js
+++ config/ambiente.js
@@ -2,7 +2,7 @@
 
 //Cambiar atributo enviar_mensajes : true, si se va a produccion
 //env : 'PRODUCTION', si se va a produccion
-
+/*
 module.exports = {
     configuracion: {
         'secret': 'supersecretDevelop',
@@ -10,8 +10,8 @@ module.exports = {
         enviar_mensajes: false
     }
 };
+*/
 
-/*
 module.exports = {
     configuracion: {
         'secret': 'supersecret',
@@ -19,5 +19,5 @@ module.exports = {
         enviar_mensajes: true
     }
 };
-*/
+
 //psql --set=sslmode=require -h ec2-54-243-197-120.compute-1.amazonaws.com -p 5432 -U vbkxmhcwhsnoxe -d d3cledlk3uuch6
