d236d4ed11da09b7cb86c31ef551863274c99fea
jorodriguez <joel.rod.roj@hotmail.com>
2020-01-06 15:00:44 -0600

    Cambios de parametros de conexcion

--- db/conexion.js
+++ db/conexion.js
@@ -4,9 +4,9 @@ const Pool = require('pg').Pool
 //const { dbParams } = require('../config/config');
 
 const dbParams = {
-    user: 'vbkxmhcwhsnoxe',
-    host: 'ec2-54-243-197-120.compute-1.amazonaws.com',
-    database: 'd3cledlk3uuch6',
+    user: 'guard_user_dev',
+    host: 'siscop.mpg-ihsa.com.mx',
+    database: 'guard_dev',
     password: 'ba3232ca7f43cbd24ae67032e8a29a530e7f2e50b7a68b7d9e63ff545e6d04fb',
     port: 5432,
     ssl: { rejectUnauthorized: false }
