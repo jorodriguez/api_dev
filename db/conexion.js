9f30ec0c58845af19f1b83de16c46d19407cc0f7
joel <joel@localhost.localdomain>
2020-01-12 20:14:22 -0600

    Cambios

--- db/conexion.js
+++ db/conexion.js
@@ -4,7 +4,7 @@ dotenv.config();
 
 //const { dbParams } = require('../config/config');
 
-/*
+
 const dbParams = {
     user: 'vbkxmhcwhsnoxe',
     host: 'ec2-54-243-197-120.compute-1.amazonaws.com',
@@ -13,7 +13,7 @@ const dbParams = {
     port: 5432,
     ssl: { rejectUnauthorized: false }
 }
-*/
+
 
 
 /*const dbParams = {
@@ -36,7 +36,7 @@ const dbParams = {
 }*/
      
 //db desarrollo
-
+/*
 const dbParams = {
     user: (process.env.USER_DB || 'pffyesodvpvsrp'),
     host: (process.env.HOST_DB || 'ec2-174-129-242-183.compute-1.amazonaws.com'),
@@ -44,7 +44,7 @@ const dbParams = {
     password: (process.env.PASSWORD_DB ||'f4de35950e23261169a79f8ac3007630aaefc8ff887c147b9283a8f68b165019'),
     port: (process.env.PORT_DB ||5432),
     ssl: { rejectUnauthorized: false }
-}
+}*/
 
 
 
