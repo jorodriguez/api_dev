ef1cd9367892130384c1308b35804b168cb54b60
jorodriguez <joel.rod.roj@hotmail.com>
2020-02-10 11:36:06 -0600

    cambios para actualizar ramas

--- db/conexion.js
+++ db/conexion.js
@@ -16,28 +16,19 @@ const dbParams = {
 */
 
 //Nuevos parametros a la DB
-const dbParams = {
+/*const dbParams = {
     user: 'abrrmwtzwwecjj',
     host: 'ec2-54-174-229-152.compute-1.amazonaws.com',
     database: 'd8q9p1up9bd7b7',
     password: '5fd81b93106ce7208d5456059519a5f3d5c29870600bfc63d820f1616540f295',
     port: 5432,
     ssl: { rejectUnauthorized: false }
-}
+}*/
 
-/*const dbParams = {
-    user: 'guard_user_dev',
-    host: 'siscop.mpg-ihsa.com.mx',
-    database: 'guard_dev',
-    password: 'ba3232ca7f43cbd24ae67032e8a29a530e7f2e50b7a68b7d9e63ff545e6d04fb',
-    port: 5432,
-    ssl:  { rejectUnauthorized: false }
-}
-*/
 
 
 //db desarrollo
-/*
+
 const dbParams = {
     user: (process.env.USER_DB || 'pffyesodvpvsrp'),
     host: (process.env.HOST_DB || 'ec2-174-129-242-183.compute-1.amazonaws.com'),
@@ -46,7 +37,7 @@ const dbParams = {
     port: (process.env.PORT_DB ||5432),
     ssl: { rejectUnauthorized: false }
 }
-*/
+
 
 
 /*
