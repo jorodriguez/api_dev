d085c457b195edd45a3a169183dae4764c8fbf40
jorodriguez <joel.rod.roj@hotmail.com>
2020-02-14 13:43:09 -0600

    Cambios

--- dao/usuarioDao.js
+++ dao/usuarioDao.js
@@ -1,4 +1,6 @@
 const genericDao = require('./genericDao');
+const { TIPO_USUARIO } = require('../utils/Constantes');
+const bcrypt = require('bcryptjs');
 
 function obtenerCorreosPorTema(co_sucursal, id_tema) {
     return genericDao.findAll(`
@@ -15,5 +17,60 @@ function obtenerCorreosPorTema(co_sucursal, id_tema) {
 `, [co_sucursal, id_tema])
 }
 
+const getUsuarioPorSucursal = (idSucursal) => {
+    return genericDao.findAll(`SELECT `);
+};
 
-module.exports = {obtenerCorreosPorTema}
+falta generarle una clave de acceso y guardarla en el insert
+
+const insertarUsuario = (usuarioData) => {
+    console.log("@insertarUsuario");
+
+    const { nombre, correo, id_sucursal, hora_entrada, hora_salida, genero } = cargoData;
+    
+    //TIPO_USUARIO.MAESTRA
+    console.log(" hora entrada " + hora_entrada + " h salida " + hora_salida + " correo " + correo);
+    
+    let sql = `
+            INSERT INTO USUARIO(NOMBRE,CORREO,CO_SUCURSAL,CO_TIPO_USUARIO,HORA_ENTRADA,HORA_SALIDA,GENERO)
+            VALUES($1,$2,$3,$4,$5,$6,$7);
+            `;
+    return genericDao.execute(sql, [nombre,correo,id_sucursal,TIPO_USUARIO.MAESTRA,hora_entrada,hora_entrada,genero]);        
+};
+
+
+const modificarUsuario = (usuarioData) => {
+    console.log("@modificarUsuario");
+
+    const { nombre, correo, hora_entrada, hora_salida, genero } = cargoData;
+    
+    //TIPO_USUARIO.MAESTRA
+    console.log(" hora entrada " + hora_entrada + " h salida " + hora_salida + " correo " + correo);
+    
+    let sql = `
+            INSERT INTO USUARIO(NOMBRE,CORREO,CO_SUCURSAL,CO_TIPO_USUARIO,HORA_ENTRADA,HORA_SALIDA,GENERO)
+            VALUES($1,$2,$3,$4,$5,$6);
+            `;
+    return genericDao.execute(sql, [nombre,correo,hora_entrada,hora_entrada,genero]);        
+};
+
+
+const bajaUsuario = (usuarioData) => {
+
+    console.log("@modificarUsuario");
+
+    const {id_usuario,motivo_baja,fecha_baja, genero } = cargoData;
+    let sql = `
+            UPTADE USUARIO SET 
+                    BAJA = TRUE,
+                    MOTIVO_BAJA = $2,
+                    FECHA_BAJA = $3,
+                    FECHA_MODIFICO=getDate(''),
+                    MODIFICO = $4
+            WHERE ID = $1;                    
+            `;
+    return genericDao.execute(sql, [id_usuario,motivo_baja,fecha_baja,genero]);        
+
+};
+
+module.exports = { obtenerCorreosPorTema,insertarUsuario,modificarUsuario,eliminarUsuario,bajaUsuario}
