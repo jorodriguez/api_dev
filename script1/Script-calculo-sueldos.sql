79df8732ff0002f63545cc941c12ea13eacaa83f
Joel Rodriguez Rojas <jorodriguez@ihsa.mx>
2020-10-07 18:31:15 -0500

    Cambios, solo se modifico el query que obtiene las faltas y se agreo el sueldo quincenal

---
+++ script1/Script-calculo-sueldos.sql
@@ -0,0 +1,48 @@
+
+alter table usuario add column sueldo_mensual numeric;
+alter table usuario add column sueldo_quincenal numeric;
+update usuario set sueldo_mensual = 0;
+update usuario set sueldo_quincenal = 0;
+alter table usuario alter column sueldo_mensual set not null;
+alter table usuario alter column sueldo_quincenal set not null;
+
+
+
+---query que calcula sueldos y descuentos por dias 
+
+with dias_activos_trabajados AS(		
+            SELECT count(*) as dias_laborables
+            FROM  generate_series('2020-09-01'::date,'2020-09-15'::date,'1 day')  g
+            WHERE g::date not in (select fecha 
+                                    from cat_dias_asueto 
+                                    where cat_empresa = 1
+                                        and fecha between '2020-09-01'::date  and '2020-09-15'::date
+                                        and activo=true 
+                                        and eliminado = false)
+                  and to_char(g::date,'d')::int not in (1,7)
+        ) select 
+            u.id,
+            u.nombre as usuario,
+            u.hora_entrada::text,
+            u.hora_salida::text,
+			ROUND(u.sueldo_mensual,2) as sueldo_base_mensual,			
+			ROUND(u.sueldo_quincenal,2) as sueldo_base_quincenal,
+		    ((d.dias_laborables - count(au.id)) * 100) / d.dias_laborables as porcentaje_falta,						
+			ROUND(
+					(
+						u.sueldo_quincenal - (u.sueldo_quincenal * ((d.dias_laborables::numeric - count(au.id)::numeric) / d.dias_laborables))
+					)
+				  ,2) as sueldo_quincenal_pago,
+			ROUND(
+					(
+						u.sueldo_mensual - u.sueldo_mensual * ((d.dias_laborables - count(au.id)) / d.dias_laborables)
+					)
+				,2) as sueldo_mensual_pago,			
+			count(au.id) as count_dias_asistencia,
+            d.dias_laborables - count(au.id) as count_dias_faltas,
+            d.dias_laborables
+        from dias_activos_trabajados d, usuario u left join co_asistencia_usuario au on au.usuario = u.id 
+                                                and au.fecha between '2020-09-01'::date  and '2020-09-15'::date
+        where u.co_sucursal = 2 and u.cat_tipo_usuario = 1 and u.eliminado = false		  
+            group by u.id,d.dias_laborables
+            order by u.nombre
--- services/asistencia_usuario.js
+++ services/asistencia_usuario.js
@@ -167,7 +167,7 @@ const registrarSalidaUsuario = (request, response) => {
 
 //Lista de faltas por rango de fecha y sucursal
 //FIXME: el id de la empresa debe de enviarse por parametro
-const getListaFaltasUsuariosSucursalRangoFecha = (request, response) => {
+/*const getListaFaltasUsuariosSucursalRangoFecha = (request, response) => {
     console.log("@getListaFaltasUsuariosSucursalRangoFecha");
 
     const { id_sucursal, fecha_inicio, fecha_fin, id_empresa } = request.params;
@@ -207,8 +207,61 @@ const getListaFaltasUsuariosSucursalRangoFecha = (request, response) => {
     } catch (e) {
         handle.callbackErrorNoControlado(e, response);
     }
-};
+};*/
+const getListaFaltasUsuariosSucursalRangoFecha = (request, response) => {
+    console.log("@getListaFaltasUsuariosSucursalRangoFecha");
 
+    const { id_sucursal, fecha_inicio, fecha_fin, id_empresa } = request.params;
+
+    console.log("id_suc = " + id_sucursal);
+    console.log("fecha = " + fecha_inicio + " fecha fin " + fecha_fin);
+
+    try {
+        getResultQuery(` 
+        with dias_activos_trabajados AS(		
+            SELECT count(*) as dias_laborables			  
+            FROM  generate_series($3::date,$4::date,'1 day')  g
+            WHERE g::date not in (select fecha 
+                                    from cat_dias_asueto 
+                                    where cat_empresa = $5 
+                                        and fecha between $3::date  and $4::date
+                                        and activo=true 
+                                        and eliminado = false)
+                  and to_char(g::date,'d')::int not in (1,7)
+        ) select 
+            u.id,
+            u.nombre as usuario,
+            u.hora_entrada::text,
+            u.hora_salida::text,
+            ROUND(u.sueldo_mensual,2) as sueldo_base_mensual,			
+			ROUND(u.sueldo_quincenal,2) as sueldo_base_quincenal,
+		    ((d.dias_laborables - count(au.id)) * 100) / d.dias_laborables as porcentaje_falta,						
+			ROUND(
+					(
+						u.sueldo_quincenal - (u.sueldo_quincenal * ((d.dias_laborables::numeric - count(au.id)::numeric) / d.dias_laborables))
+					)
+				  ,2) as pago_sueldo_quincenal,
+			ROUND(
+					(
+						u.sueldo_mensual - u.sueldo_mensual * ((d.dias_laborables - count(au.id)) / d.dias_laborables)
+					)
+				,2) as pago_sueldo_mensual,			
+			count(au.id) as count_dias_asistencia,
+            d.dias_laborables - count(au.id) as count_dias_faltas,
+            d.dias_laborables
+        from dias_activos_trabajados d, usuario u left join co_asistencia_usuario au on au.usuario = u.id 
+                                                and au.fecha between $3::date  and $4::date
+        where u.co_sucursal = $1 and u.cat_tipo_usuario = $2 and u.eliminado = false		  
+            group by u.id,d.dias_laborables
+            order by u.nombre
+     `, [id_sucursal, TIPO_USUARIO.MAESTRA, new Date(fecha_inicio), new Date(fecha_fin), (id_empresa || ID_EMPRESA_MAGIC)],            
+            response
+        );
+
+    } catch (e) {
+        handle.callbackErrorNoControlado(e, response);
+    }
+};
 
 const getDetalleFaltasUsuariosRangoFecha = (request, response) => {
     console.log("@getDetalleFaltasUsuariosRangoFecha");
