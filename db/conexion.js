b9ed8ff452b63d3bb0caa5b1fae383b3bb47f256
joel <joel.rod.roj@hotmail.com>
2020-10-14 21:10:04 -0500

    Subiendo el cambio que calcula sueldos

--- db/conexion.js
+++ db/conexion.js
@@ -2,22 +2,7 @@ const Pool = require('pg').Pool
 const dotenv = require('dotenv');
 dotenv.config();
 
-//const { dbParams } = require('../config/config');
-
-/*
-const dbParams = {
-    user: 'vbkxmhcwhsnoxe',
-    host: 'ec2-54-243-197-120.compute-1.amazonaws.com',
-    database: 'd3cledlk3uuch6',
-    password: 'ba3232ca7f43cbd24ae67032e8a29a530e7f2e50b7a68b7d9e63ff545e6d04fb',
-    port: 5432,
-    ssl: { rejectUnauthorized: false }
-}
-*/
-
 //Nuevos parametros a la DB
-
-/*
 const dbParams = {
    user: 'abrrmwtzwwecjj',
     host: 'ec2-54-174-229-152.compute-1.amazonaws.com',
@@ -25,11 +10,11 @@ const dbParams = {
     password: '5fd81b93106ce7208d5456059519a5f3d5c29870600bfc63d820f1616540f295',
     port: 5432,
     ssl: { rejectUnauthorized: false }
-};*/
+};
 
 
 //db desarrollo
-
+/*
 const dbParams = {
     user: (process.env.USER_DB || 'pffyesodvpvsrp'),
     host: (process.env.HOST_DB || 'ec2-174-129-242-183.compute-1.amazonaws.com'),
@@ -37,7 +22,7 @@ const dbParams = {
     password: (process.env.PASSWORD_DB ||'f4de35950e23261169a79f8ac3007630aaefc8ff887c147b9283a8f68b165019'),
     port: (process.env.PORT_DB ||5432),
     ssl: { rejectUnauthorized: false }
-};
+};*/
 
 
 
