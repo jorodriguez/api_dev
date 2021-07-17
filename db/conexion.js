2c93991f8412a1b18a923e0a8a20992959a7bf3b
joel.rod.roj@hotmail.com <joel.rod.roj@hotmail.com>
2021-07-17 00:00:27 -0500

    cambiando para que apunte a la db de test

--- db/conexion.js
+++ db/conexion.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 //Nuevos parametros a la DB
-
+/*
 const dbParams = {
    user: 'abrrmwtzwwecjj',
     host: 'ec2-54-174-229-152.compute-1.amazonaws.com',
@@ -11,7 +11,7 @@ const dbParams = {
     password: '5fd81b93106ce7208d5456059519a5f3d5c29870600bfc63d820f1616540f295',
     port: 5432,
     ssl: { rejectUnauthorized: false }
-};
+};*/
 
 
 //production
@@ -26,7 +26,7 @@ const dbParams = {
  };*/
 
 //db desarrollo
-/*
+
 const dbParams = {
     user: (process.env.USER_DB || 'pffyesodvpvsrp'),
     host: (process.env.HOST_DB || 'ec2-174-129-242-183.compute-1.amazonaws.com'),
@@ -35,7 +35,7 @@ const dbParams = {
     port: (process.env.PORT_DB ||5432),
     ssl:true
    // ssl: { rejectUnauthorized: false }
-};*/
+};
 
 /*
 const dbParams = {
