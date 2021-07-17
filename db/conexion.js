b96a5c44348b83586dcbb259c4b81932b7aeb428
joel.rod.roj@hotmail.com <joel.rod.roj@hotmail.com>
2021-07-16 20:55:00 -0500

    Apunta a testing para demo

--- db/conexion.js
+++ db/conexion.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 //Nuevos parametros a la DB
-
+/*
 const dbParams = {
    user: 'abrrmwtzwwecjj',
     host: 'ec2-54-174-229-152.compute-1.amazonaws.com',
@@ -11,7 +11,7 @@ const dbParams = {
     password: '5fd81b93106ce7208d5456059519a5f3d5c29870600bfc63d820f1616540f295',
     port: 5432,
     ssl: { rejectUnauthorized: false }
-};
+};*/
 
 
 //production
@@ -37,7 +37,7 @@ const dbParams = {
    // ssl: { rejectUnauthorized: false }
 };*/
 
-/*
+
 const dbParams = {
     user: (process.env.USER_DB || 'swxmnyur'),
     host: (process.env.HOST_DB || 'batyr.db.elephantsql.com'),
@@ -47,7 +47,7 @@ const dbParams = {
     ssl:true
    // ssl: { rejectUnauthorized: false }
 };
-*/
+
 //const dbParams = dbParamsDev;
 
 const pool = new Pool({
