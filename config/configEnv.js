8e165968df7793567f18d541aa23b6f72100d9e4
Joel Rodriguez Rojas <jorodriguez@ihsa.mx>
2021-08-18 13:16:53 -0500

    cambios en los parametros

--- config/configEnv.js
+++ config/configEnv.js
@@ -1,5 +1,7 @@
+const dotenv = require('dotenv').config();
+
 module.exports = {
-    NODE_ENV: process.env.NODE_ENV || 'local_development',
+    ENV: process.env.ENV || 'local_development',
     HOST: process.env.HOST || '127.0.0.1',
     PORT: process.env.PORT || 5000,
     USER_DB: process.env.USER_DB || '',
